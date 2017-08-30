2defb25f352dfcbe4ef38c66b3522b71700c1ab3
raynamharris <rayna.harris@utexas.edu>
2017-08-30 17:05:08 -0500

    new project

---
+++ scripts/00_cpfromBehavEphyRNAseq.sh
@@ -0,0 +1,5 @@
+# mv 02f_wgcna_RNAseq.R ../../FMR1rnaseqCA1/00_wgcna.R
+cp ../../BehavEphyRNAseq/data/rnaseq/fmr1* .
+cp ../../BehavEphyRNAseq/data/rnaseq/FMR1MetaData.xls .
+cp ../../BehavEphyRNAseq/data/rnaseq/geneids.csv .
+cp -r ../../BehavEphyRNAseq/data/rnaseq/JA17009/ .

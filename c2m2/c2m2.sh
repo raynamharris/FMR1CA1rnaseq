9f1873f25d4a175ac5a4d959f513d455787deb36
Rayna M Harris <raynamharris@users.noreply.github.com>
2022-04-08 13:16:07 -0700

    add bash script

--- c2m2/c2m2.sh
+++ c2m2/c2m2.sh
@@ -2,4 +2,4 @@
 
 cp blank_nonCV_C2M2_tables/biosample_in_collection.tsv rmh_2022_04_08/
 cp blank_nonCV_C2M2_tables/biosample_substance.tsv rmh_2022_04_08/
-cp blank_nonCV_C2M2_tables/collection* rmh_2022_04_08/
+cp blank_nonCV_C2M2_tables/collection* rmh_2022_04_08/git s
